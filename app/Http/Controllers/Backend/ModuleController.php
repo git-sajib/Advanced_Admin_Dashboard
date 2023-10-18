31fdde5071606d4e39a7df3292dce8986b48a2f6
Samiul Sajib <40036775+git-sajib@users.noreply.github.com>
2023-10-18 23:07:04 +0600

    Module create page and store to db

--- app/Http/Controllers/Backend/ModuleController.php
+++ app/Http/Controllers/Backend/ModuleController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ModuleStoreRequest;
 use App\Models\Module;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ModuleController extends Controller
 {
@@ -28,9 +30,14 @@ class ModuleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ModuleStoreRequest $request)
     {
-        //
+        Module::updateOrCreate([
+            'module_name' => $request->module_name,
+            'module_slug' => Str::slug($request->module_name),
+        ]);
+
+        return redirect()->route('module.index');
     }
 
     /**
