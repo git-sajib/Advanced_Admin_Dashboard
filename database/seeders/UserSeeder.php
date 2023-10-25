9392c179789d2f7003610b0e4b09c01e3f8059ea
Samiul Sajib <40036775+git-sajib@users.noreply.github.com>
2023-10-26 00:32:17 +0600

    New manager user create using seeder and error page customization

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -16,23 +17,65 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create Admin
+        // DB::table('users')->truncate(); //run specific seeder class Ex: "php artisan db:seed --class=UserSeeder"
+        // Create Admin1
         $adminRoleId = Role::where('role_slug', 'admin')->first()->id;
         User::UpdateOrCreate([
             'role_id' => $adminRoleId,
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
+            'name' => 'Admin1',
+            'email' => 'admin1@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('1234'), //1234
             'remember_token' => Str::random(10),
         ]);
 
-        //Create User
+        // Create Admin2
+        User::UpdateOrCreate([
+            'role_id' => $adminRoleId,
+            'name' => 'Admin2',
+            'email' => 'admin2@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234'), //1234
+            'remember_token' => Str::random(10),
+        ]);
+
+        //Create User1
         $userRoleId = Role::where('role_slug', 'user')->first()->id;
         User::UpdateOrCreate([
             'role_id' => $userRoleId,
-            'name' => 'User',
-            'email' => 'user@gmail.com',
+            'name' => 'User1',
+            'email' => 'user1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234'), //1234
+            'remember_token' => Str::random(10),
+        ]);
+
+        //Create User2
+        User::UpdateOrCreate([
+            'role_id' => $userRoleId,
+            'name' => 'User2',
+            'email' => 'user2@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234'), //1234
+            'remember_token' => Str::random(10),
+        ]);
+
+        //Create Manager1
+        $userRoleId = Role::where('role_slug', 'manager')->first()->id;
+        User::UpdateOrCreate([
+            'role_id' => $userRoleId,
+            'name' => 'Manager1',
+            'email' => 'manager1@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234'), //1234
+            'remember_token' => Str::random(10),
+        ]);
+
+        //Create Manager2
+        User::UpdateOrCreate([
+            'role_id' => $userRoleId,
+            'name' => 'Manager2',
+            'email' => 'manager2@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('1234'), //1234
             'remember_token' => Str::random(10),
