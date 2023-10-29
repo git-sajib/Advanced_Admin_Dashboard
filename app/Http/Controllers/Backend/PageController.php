74a906a34c8d467c384f5210baa2447ed59f5204
Samiul Sajib <40036775+git-sajib@users.noreply.github.com>
2023-10-29 23:35:39 +0600

    Page edit update and delete with image

--- app/Http/Controllers/Backend/PageController.php
+++ app/Http/Controllers/Backend/PageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PageStoreRequest;
 use App\Http\Requests\PageStroeRequest;
+use App\Http\Requests\PageUpdateRequest;
 use App\Models\Page;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Str;
@@ -65,15 +66,31 @@ class PageController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $page = Page::find($id);
+        return view('admin.pages.page-builder.edit', compact('page'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PageUpdateRequest $request, string $id)
     {
-        //
+        $page = Page::find($id);
+        $page->update([
+            'page_title' => $request->page_title,
+            'page_slug' => $request->page_slug ?? Str::slug($request->page_title),
+            'page_short' => $request->page_short,
+            'page_long' => $request->page_long,
+            'meta_title' => $request->meta_title,
+            'meta_keywords' => $request->meta_keywords,
+            'meta_description' => $request->meta_description,
+            'is_active' => true,
+        ]);
+
+        $this->image_upload($request, $page->id);
+
+        Toastr::success('Page Updated Successfully');
+        return redirect()->route('page.index');
     }
 
     /**
@@ -81,7 +98,15 @@ class PageController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $page = Page::find($id);
+        //delete old photo
+        if ($page->page_image != null) {
+            $old_photo_path = 'public/uploads/page_images/' . $page->page_image;
+            unlink(base_path($old_photo_path));
+        }
+        $page->delete();
+        Toastr::success('Page Deleted Successfully.');
+        return redirect()->route('page.index');
     }
 
     function checkActive($page_id)
@@ -119,7 +144,7 @@ class PageController extends Controller
             $new_photo_location = $photo_location . $new_photo_name; //Ex: public/uploads/page_images/1.jpg
 
             //save image
-            Image::make($uploaded_photo)->resize(600, 600)->save(base_path($new_photo_location));
+            Image::make($uploaded_photo)->save(base_path($new_photo_location));
             $page->update([
                 'page_image' => $new_photo_name,
             ]);
---
+++ app/Http/Requests/PageUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PageUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'page_title' => 'required|string|max:255',
+            'page_slug' => 'nullable|string|max:255',
+            'page_short' => 'nullable|string',
+            'page_long' => 'nullable|string',
+            'page_image' => 'nullable|image|mimes:png,jpg',
+            'meta_title' => 'nullable|string|max:255',
+            'meta_keywords' => 'nullable|string|max:255',
+            'meta_description' => 'nullable|string|max:255',
+        ];
+    }
+}
